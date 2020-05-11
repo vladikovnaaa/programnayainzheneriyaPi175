fa6fbc143c4c190caa72d81dc3ccb3969d072285
vladikovnaaa <55970615+vladikovnaaa@users.noreply.github.com>
2020-05-11 11:17:57 +0500

    Update app.js

--- 2 laba/app.js
+++ 2 laba/app.js
@@ -2,18 +2,24 @@
     var express = require('express');
     var app = express();
 
-    var server = app.listen(7998, function () {
+    var server = app.listen(7998, function ()
+    {
     var host = server.address().address
     var port = server.address().port
 
-  function connect()
- {
- 	let connecti = mysql.createConnection({
- 		host: 'localhost',
- 		user: 'root',
- 		password: '',
- 		database: 'Laba2'
- 		});		
- 	connecti.connect();
- 	return connecti;
- }
+        console.log("Example app listening at http://%s:%s", host, port)
+    })
+    var mysql = require('mysql');
+
+
+    function connect()
+    {
+        var connecti = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'laba2'
+        });
+        connecti.connect();
+        return connecti;
+    }
