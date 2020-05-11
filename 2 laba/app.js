b042b207b4c5cc93a982792283a189450fcd032b
vladikovnaaa <55970615+vladikovnaaa@users.noreply.github.com>
2020-05-11 11:50:24 +0500

    Update app.js

--- 2 laba/app.js
+++ 2 laba/app.js
@@ -1,6 +1,7 @@
     //Лабораторная работа №2
     var express = require('express');
     var app = express();
+    var mysql = require('mysql');
 
     var server = app.listen(7998, function ()
     {
@@ -9,7 +10,6 @@
 
         console.log("Example app listening at http://%s:%s", host, port)
     })
-    var mysql = require('mysql');
 
     function connect()
     {
@@ -34,4 +34,23 @@
             resil.send(resultate[0]);
         });
         connecti.end();
+    });
+
+    //paste http://localhost:7998/pas/?table=service&parametr1=repairs&parametr2=111
+    app.get("/pas", (req, resil)=>
+    {
+        var connecti = connect();
+        var tables = req.query.table;
+        var parametr1 = req.query.parametr1;
+        var parametr2 = req.query.parametr2;
+
+        if (parametr1 == undefined) parametr1 = '-';
+        if (parametr2 == undefined) parametr2 = '-';
+
+        connecti.query("call proc(?,?,?,?,?)", [tables, 'pas', parametr1, parametr2, '-'], function(errors, resultate) {
+            if (errors) throw errors;
+            resil.send("Данные добавлены");
+        });
+        connecti.end();
+
     });
