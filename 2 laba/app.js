827fec7f314a46c492b28ff99dedd9e07aa85995
vladikovnaaa <55970615+vladikovnaaa@users.noreply.github.com>
2020-05-11 17:24:13 +0500

    Update app.js

--- 2 laba/app.js
+++ 2 laba/app.js
@@ -29,7 +29,7 @@
     {
         var connecti = connect();
         var tables = req.query.table;
-        connecti.query("call proc(?,?,?,?,?)", [tables, 'pol', '-', '-', '-'], function(errors, resultate) {
+        connecti.query("call procedure(?,?,?,?,?)", [tables, 'pol', '-', '-', '-'], function(errors, resultate) {
             if (errors) throw errors;
             resil.send(resultate[0]);
         });
@@ -47,10 +47,37 @@
         if (parametr1 == undefined) parametr1 = '-';
         if (parametr2 == undefined) parametr2 = '-';
 
-        connecti.query("call proc(?,?,?,?,?)", [tables, 'pas', parametr1, parametr2, '-'], function(errors, resultate) {
+        connecti.query("call procedure(?,?,?,?,?)", [tables, 'pas', parametr1, parametr2, '-'], function(errors, resultate) {
             if (errors) throw errors;
-            resil.send("Данные добавлены");
+            resil.send(" Информация добавлена");
         });
         connecti.end();
+    });
 
+    //remove http://localhost:7998/rem/?table=service&numb=2
+    app.get("/rem", (req, resil)=>
+    {
+        var connecti = connect();
+        var tables = req.query.table;
+        var numb = req.query.numb;
+        connecti.query("call procedure(?,?,?,?,?)", [tables, 'rem', numb, '-', '-'], function(errors, resultate) {
+            if (errors) throw errors;
+            resil.send("Информация удалена");
+        });
+        connecti.end();
+    });
+
+    //update http://localhost:7998/chan/?table=service&parametr1=cleaning&parametr2=1000&numb=1
+    app.get("/chan", (req, resil)=>
+    {
+        var connecti = connect();
+        var tables = req.query.table;
+        var parametr1 = req.query.parametr1;
+        var parametr2 = req.query.parametr2;
+        var numb = req.query.numb;
+        connecti.query("call procedure(?,?,?,?,?)", [tables, 'chan', parametr1, parametr2, numb], function(errors, resultate) {
+            if (errors) throw errors;
+            resil.send("Информация обновлена");
+        });
+        connecti.end();
     });
